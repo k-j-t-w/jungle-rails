1f1f3fef6ff2172d7e3114b60ce0d41de9050cb1
Kai Wingfield <kaiwingfield17@gmail.com>
2024-05-11 16:00:29 -0700

    Add basic admin authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class Admin::DashboardController < ApplicationController
+  Dotenv::Railtie.load
+
+  before_action :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASS']
+    end
+  end
+  
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,13 @@
 class Admin::ProductsController < ApplicationController
+  Dotenv::Railtie.load
+
+  before_action :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASS']
+    end
+  end
 
   def index
     @products = Product.order(id: :desc).all
