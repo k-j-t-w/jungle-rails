d0b8e1a5d4d38a610850702d34483521c01ec74c
Kai Wingfield <kaiwingfield17@gmail.com>
2024-05-25 13:40:06 -0700

    add home page testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
         /* ... */
       })
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,tsx,ts}', // Add this line
   },
   screenshotsFolder: "tmp/cypress_screenshots",
   videosFolder: "tmp/cypress_videos",
-  trashAssetsBeforeRuns: false
+  trashAssetsBeforeRuns: false,
 })
---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,19 @@
+describe('Home page', () => {
+  beforeEach(() => {
+
+    cy.visit('/')
+  })
+
+  it('successfully loads', () => {
+    cy.visit('/')
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+})
